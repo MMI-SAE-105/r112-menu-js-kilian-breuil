af516dd7f90f644f7ba7e708c560d45276f95a57
kilian-breuil <150059078+kilian-breuil@users.noreply.github.com>
2024-11-13 09:47:58 +0100

    classe no scroll

--- src/js/script.js
+++ src/js/script.js
@@ -7,8 +7,10 @@ toggle.addEventListener("click", () => {
     const expanded = toggle.getAttribute("aria-expanded") === "true" || false;
     toggle.setAttribute("aria-expanded", !expanded);
     nav.hidden = expanded;
+    document.body.classList.add('no-scroll');
     const ariaHidden = nav.getAttribute("aria-hidden") === "true" || false;
     nav.setAttribute("aria-hidden", !ariaHidden);
+    document.body.classList.remove('no-scroll');
   // Mise à jour des attributs ARIA pour accessibilité
   nav__________________;
   toggle__________________;
